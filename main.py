2ae292ed31b7e387fff152c4f483e2a98d332cad
abdulrashaad <rashardharuna16@gmail.com>
2023-12-19 16:54:37 +0000

    This is my first commit message for python

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    print('Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
+
+from library import greet
+
+
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    main()
