a66c59ef1fded3a7834ac2a0c5474c9e26d46d31
abdulrashaad <rashardharuna16@gmail.com>
2023-12-26 15:10:10 +0000

    This is my first commit message for pythonbb

---
+++ name_input.py
@@ -0,0 +1,20 @@
+def main():
+    name = input('What is your name? ')
+    print(f'Nice to meet you {name}')
+
+    age = int(input(f'How old are you? {name}'))
+    current_year = int(input(f'What is this year again?'))
+    hometown = input('Where are you from?')
+    print(f'I am from {hometown}')
+    sex = input('are a male or female?')
+    print(f'i am a {sex}')
+
+
+
+
+
+    print(f'If my calculations are right you wer born in the year: {current_year - age}')
+
+
+if __name__ == '__main__':
+    main()
